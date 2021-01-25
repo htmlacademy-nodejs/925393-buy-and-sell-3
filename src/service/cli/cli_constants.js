c2ea16df428f0704055e91adb03dccfed19a04ce
Eugene Koretsky <E.Koretsky@outlook.com>
2021-01-25 16:59:18 +0300

    Рефакторирг cli_constants.js

--- src/service/cli/cli_constants.js
+++ src/service/cli/cli_constants.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const path = require(`path`);
+
 module.exports.DEFAULT_COMMAND = `--help`;
 module.exports.DEFAULT_COUNT = 1;
 module.exports.FILE_NAME = `mocks.json`;
@@ -16,9 +18,9 @@ module.exports.RESTRICT = {
   max: 100000
 };
 module.exports.DATA_PATH = {
-  FILE_SENTENCES_PATH: `../../data/sentences.txt`,
-  FILE_TITLES_PATH: `../../data/titles.txt`,
-  FILE_CATEGORIES_PATH: `../../data/categories.txt`,
+  FILE_SENTENCES_PATH: path.join(process.env.NODE_PATH, `data`, `sentences.txt`),
+  FILE_TITLES_PATH: path.join(process.env.NODE_PATH, `data`, `titles.txt`),
+  FILE_CATEGORIES_PATH: path.join(process.env.NODE_PATH, `data`, `categories.txt`),
 };
 
 module.exports.OFFER_TYPE = [
