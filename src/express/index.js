e0544460890866f12ebbbdc0a511a16beef68a6c
Eugene Koretsky <E.Koretsky@outlook.com>
2021-02-01 01:52:47 +0300

    Добавлена логика для express

---
+++ src/express/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require(`express`);
+const routes = require(`./routes`)
+
+const DEFAULT_PORT = 8080;
+const app = express();
+
+app.use(`/`, routes.mainRoutes);
+app.use(`/offers`, routes.offersRoutes);
+app.use(`/my`, routes.myRoutes);
+
+app.listen(DEFAULT_PORT, () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`))
