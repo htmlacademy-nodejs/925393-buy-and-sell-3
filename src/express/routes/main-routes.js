ba144bd0c36a39bc69bf2fe035af179717fa5365
Eugene Koretsky <E.Koretsky@outlook.com>
2021-02-03 21:25:51 +0300

    Небольшие фиксы роутов

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -4,8 +4,16 @@ const {Router} = require(`express`);
 
 const mainRoutes = new Router();
 
-mainRoutes.get(`/`, (req, res) => console.log(`main`));
-mainRoutes.get(`/login`, (req, res) => console.log(`/login`));
+mainRoutes.get(`/`, (req, res) => {
+  res.render('pages/main/index', {
+    title: 'rvrrvrv'
+  })
+  console.log(`main`)
+});
+mainRoutes.get(`/login`, (req, res) => {
+  res.render('pages/login/login')
+  console.log(`/login`)
+});
 mainRoutes.get(`/register`, (req, res) => console.log(`/register`));
 mainRoutes.get(`/search`, (req, res) => console.log(`/search`));
 
