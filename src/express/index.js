2c0bdf70de862a712bf651c24c54cb27481def44
Eugene Koretsky <E.Koretsky@outlook.com>
2021-02-03 21:15:35 +0300

    подключены middleware

--- src/express/index.js
+++ src/express/index.js
@@ -1,10 +1,20 @@
 "use strict";
 
+const path = require(`path`);
 const express = require(`express`);
 const routes = require(`./routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = path.resolve(__dirname, `public`)
+const TEMPLATES_DIR = path.resolve(__dirname, `templates`)
+console.log(PUBLIC_DIR)
+
 const app = express();
+
+//middleware
+app.use(express.static(PUBLIC_DIR))
+app.set(`views`, TEMPLATES_DIR)
+app.set(`view engine`, `pug`);
 
 app.use(`/`, routes.mainRoutes);
 app.use(`/offers`, routes.offersRoutes);
