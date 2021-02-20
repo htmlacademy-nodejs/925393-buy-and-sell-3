d3da46707ec1a9f0fe2a3ae29e79b18f6bfee174
Eugene Koretsky <E.Koretsky@outlook.com>
2021-02-20 06:08:06 +0300

    Вынес роут в отдельный файл

--- src/service/cli/commands/server.js
+++ src/service/cli/commands/server.js
@@ -1,31 +1,17 @@
 "use strict";
 
 const chalk = require(`chalk`);
-const fs = require(`fs`).promises;
-
 const express = require(`express`);
-const {Router} = require(`express`);
+const routes = require(`../routes`);
 
 const {StatusCodes} = require(`http-status-codes`);
-const {DEFAULT_PORT, FILE_NAME} = require(`../cli_constants`);
+const {DEFAULT_PORT} = require(`../cli_constants`);
 
 const app = express();
 
-const offersRoute = new Router();
-
-offersRoute.get(`/`, async (req, res) => {
-  try {
-    const fileContent = await fs.readFile(FILE_NAME);
-    const mocks = JSON.parse(fileContent);
-    res.send(mocks);
-  } catch (err) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send([]);
-  }
-})
-
 app.use(express.json());
 
-app.use(`/offers`, offersRoute);
+app.use(`/offers`, routes.offersRoute);
 
 app.use((req, res) => res
   .status(StatusCodes.NOT_FOUND)
@@ -35,7 +21,6 @@ module.exports = {
   name: `--server`,
   async run(arg) {
     const [customPort] = arg;
-
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
     app.listen(port, () => {
