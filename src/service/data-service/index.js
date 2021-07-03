95df372ec054acfd45c21f1f734480e862cb131b
E.Koretsky <E.Koretsky@outlook.com>
2021-07-03 17:52:52 +0300

    индекс файл для файлов сервиса

---
+++ src/service/data-service/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const CategoryService = require(`./category_service`);
+const CommentsService = require(`./comments_service`);
+const OffersService = require(`./offers_service`);
+
+module.exports = {
+  CategoryService,
+  CommentsService,
+  OffersService
+};
