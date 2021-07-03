5dc2019199890153c478c0604f09272c72e22c95
E.Koretsky <E.Koretsky@outlook.com>
2021-07-03 17:49:35 +0300

    Добавлена константа для максимального количества объявлений

--- src/service/cli/cli_constants.js
+++ src/service/cli/cli_constants.js
@@ -14,6 +14,7 @@ module.exports.EXIT_CODE = {
 };
 
 module.exports.MAX_ID_LENGTH = 6;
+module.exports.MAX_COMMENTS = 4;
 
 module.exports.RESTRICT = {
   min: 1000,
@@ -23,6 +24,7 @@ module.exports.DATA_PATH = {
   FILE_SENTENCES_PATH: path.join(process.env.NODE_PATH, `data`, `sentences.txt`),
   FILE_TITLES_PATH: path.join(process.env.NODE_PATH, `data`, `titles.txt`),
   FILE_CATEGORIES_PATH: path.join(process.env.NODE_PATH, `data`, `categories.txt`),
+  FILE_COMMENTS_PATH: path.join(process.env.NODE_PATH, `data`, `comments.txt`)
 };
 
 module.exports.OFFER_TYPE = [
