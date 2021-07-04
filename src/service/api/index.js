5a51d22dbdde2e54de6d3e5bd747739b194cf778
E.Koretsky <E.Koretsky@outlook.com>
2021-07-04 20:50:32 +0300

    фиксы индекс файлов

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -1,22 +1,26 @@
 "use strict";
 
-const {Router} = require(`express`);
+const Router = require(`express`);
 const offersRoutes = require(`../api/offers_routes`);
 const categoryRoutes = require(`../api/category_routes`);
+const searchRoutes = require(`../api/search_routes`);
 
 const getMockData = require(`../lib/get-mock-data`);
 
 const {
   CategoryService,
   OffersService,
+  CommentsService,
+  SearchService
 } = require(`../data-service`);
 
 const app = new Router();
 
 (async () => {
   const mockData = await getMockData();
-  offersRoutes(app, new OffersService(mockData));
+  offersRoutes(app, new OffersService(mockData), new CommentsService(mockData));
   categoryRoutes(app, new CategoryService(mockData));
+  searchRoutes(app, new SearchService(mockData));
 })();
 
 module.exports = app;
--- src/service/data-service/index.js
+++ src/service/data-service/index.js
@@ -3,9 +3,11 @@
 const CategoryService = require(`./category_service`);
 const CommentsService = require(`./comments_service`);
 const OffersService = require(`./offers_service`);
+const SearchService = require(`./search_service`);
 
 module.exports = {
   CategoryService,
   CommentsService,
-  OffersService
+  OffersService,
+  SearchService
 };
