15b259a796fa12ec622fd86fc3f17f5114735811
E.Koretsky <E.Koretsky@outlook.com>
2021-07-04 03:11:17 +0300

    fix offers_validator.js

--- src/service/middlewares/offers_validator.js
+++ src/service/middlewares/offers_validator.js
@@ -1,17 +1,18 @@
 "use strict";
+
 const {StatusCodes} = require(`http-status-codes`);
 
 const offerKeys = [`category`, `description`, `picture`, `title`, `type`, `sum`];
 
 module.exports = (req, res, next) => {
-  console.log(req.body);
   const newOffer = req.body;
   const keys = Object.keys(newOffer);
   const keysExists = offerKeys.every((key) => keys.includes(key));
 
   if (!keysExists) {
-    res.status(StatusCodes.BAD_REQUEST)
-      .send(`Bad request`);
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json(`Bad request: в объекте запроса определены не все поля`);
   }
 
   next();
