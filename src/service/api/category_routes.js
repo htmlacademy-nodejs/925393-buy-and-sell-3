7389444773f9121543df0993ac0b73547ff003e5
E.Koretsky <E.Koretsky@outlook.com>
2021-07-04 20:51:18 +0300

    переименован эндпоинт для категорий

--- src/service/api/category_routes.js
+++ src/service/api/category_routes.js
@@ -1,12 +1,12 @@
 "use strict";
 
-const {Router} = require(`express`);
+const Router = require(`express`);
 const {StatusCodes} = require(`http-status-codes`);
 
 const categoryRoute = new Router();
 
 module.exports = (app, categoryService) => {
-  app.use(`/category`, categoryRoute);
+  app.use(`/categories`, categoryRoute);
 
   categoryRoute.get(`/`, (req, res) => {
     const categories = categoryService.findAll();
